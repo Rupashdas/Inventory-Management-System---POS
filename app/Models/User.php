8a2cf204cd227bd67be2540a472130a050358df1
Rupashdas <rupash.das.40@gmail.com>
2026-01-30 02:49:05 +0600

    Add UserSeeder and update User model and factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'firstName',
         'lastName',
@@ -13,7 +17,8 @@ class User extends Model
         'password',
         'otp',
     ];
+
     protected $attributes = [
-        'otp' => '0',
+        'otp' => 0,
     ];
 }
